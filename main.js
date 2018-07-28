9513abbde6bad1f09f5d72e9a2c5b2e761dff562
lacehileman <35512856+lacehileman@users.noreply.github.com>
2018-07-28 16:20:38 -0500

    DragAndDrop
    Drag and Drop Lesson
    
    main.js edits:
        User able to import own vector data

--- main.js
+++ main.js
@@ -8,29 +8,31 @@ import TileLayer from 'ol/layer/Tile';
 import XYZSource from 'ol/source/XYZ';
 import {fromLonLat} from 'ol/proj';
 import sync from 'ol-hashed';
-
-
+import DragAndDrop from 'ol/interaction/DragAndDrop';
 
 const map = new Map({
     target: 'map-container',
-    layers: [
-        new VectorLayer({
-            source: new VectorSource({
-                format: new GeoJSON(),
-                url: './data/countries.json'
-            })
-        })
-    ],
     view: new View({
         center: [0, 0],
         zoom: 2
     })
 });
 
-sync(map);
+const source = new VectorSource();
+
+const layer = new VectorLayer({
+    source: source
+});
+map.addLayer(layer);
+
+map.addInteraction(new DragAndDrop({
+    source: source,
+    formatConstructors: [GeoJSON]
+}));
 
 navigator.geolocation.getCurrentPosition(function(pos) {
     const coords = fromLonLat ([pos.coords.longitude, pos.coords.latitude]);
     map.getView().animate({center: coords, zoom: 10});
 });
 
+sync(map);
