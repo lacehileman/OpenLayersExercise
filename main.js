1a5ba7da78d56cc27fc4c0e75c05375dae95abc0
lacehileman <35512856+lacehileman@users.noreply.github.com>
2018-07-28 01:03:57 -0500

    RenderingGeoJSON
    Rendering GeoJSON lesson
    
    Updated index.html:
        Calls to GeoJSON data
    
    Updated main.js:
        Calls to Vector files
        Stabilize image when refreshing

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 import 'ol/ol.css';
+import GeoJSON from 'ol/format/GeoJSON';
 import Map from 'ol/Map';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import XYZSource from 'ol/source/XYZ';
 import {fromLonLat} from 'ol/proj';
+import sync from 'ol-hashed';
+
+
 
 const map = new Map({
     target: 'map-container',
     layers: [
-        new TileLayer({
-            source: new XYZSource({
-                url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
+        new VectorLayer({
+            source: new VectorSource({
+                format: new GeoJSON(),
+                url: './data/countries.json'
             })
         })
     ],
@@ -19,6 +26,8 @@ const map = new Map({
         zoom: 2
     })
 });
+
+sync(map);
 
 navigator.geolocation.getCurrentPosition(function(pos) {
     const coords = fromLonLat ([pos.coords.longitude, pos.coords.latitude]);
