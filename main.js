10e384ccf8f9afe92e48ed614941644a0a736e4e
lacehileman <35512856+lacehileman@users.noreply.github.com>
2018-07-28 23:19:35 -0500

    MakingItLookNice
    Making It Look Nice Lesson
    main.js edits:
        Added in dynamic style based off geo
    img_007 displays color render

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ import Map from 'ol/Map';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZSource from 'ol/source/XYZ';
 import {fromLonLat} from 'ol/proj';
 import sync from 'ol-hashed';
 import DragAndDrop from 'ol/interaction/DragAndDrop';
 import Modify from 'ol/interaction/Modify';
 import Draw from 'ol/interaction/Draw';
 import Snap from 'ol/interaction/Snap';
+import {Fill, Stroke, Style} from 'ol/style';
+import {getArea} from 'ol/sphere';
+import colormap from 'colormap';
 
 const map = new Map({
     target: 'map-container',
@@ -21,10 +22,39 @@ const map = new Map({
     })
 });
 
+const min = 1e8;
+const max = 2e13;
+const steps = 50;
+const ramp = colormap({
+    colormap: 'blackbody',
+    nshades: steps
+});
+
+function clamp(value, low, high) {
+    return Math.max(low, Math.min(value, high));
+}
+
+function getColor(feature) {
+    const area = getArea(feature.getGeometry());
+    const f = Math.pow(clamp((area - min) / (max - min), 0, 1), 1 / 2);
+    const index = Math.round(f * (steps - 1));
+    return ramp[index];
+}
+
 const source = new VectorSource();
 
 const layer = new VectorLayer({
-    source: source
+    source: source,
+    style: function(feature) {
+        return new Style({
+            fill: new Fill({
+                color: getColor(feature)
+            }),
+            stroke: new Stroke({
+                color: 'rgba(255,255,255,0.8)'
+            })
+        });
+    }
 });
 map.addLayer(layer);
 
